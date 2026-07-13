3b5e33f73e4982e0536300e8ca883178fe934f72
Piyushkuca <piyushkuca@gmail.com>
2026-07-14 00:25:15 +0530

    Add Preferences DataStore dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // Preferences DataStore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
